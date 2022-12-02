ca8803c0ad5f921a13a21211a569b04bb5221abb
OlViks <salova.40@yandex.ru>
2022-12-02 18:08:49 +0300

    Коллекции

--- src/set/Main.java
+++ src/set/Main.java
@@ -1,80 +1,98 @@
-package pro.sky.java.src.set;
+package set;
 
-import pro.sky.java.src.set.multiply.MultiplicationTable;
-import pro.sky.java.src.set.passport.Passport;
-import pro.sky.java.src.set.passport.PassportList;
-import pro.sky.java.src.set.products.Product;
-import pro.sky.java.src.set.products.ProductList;
-import pro.sky.java.src.set.recept.Recipe;
-import pro.sky.java.src.set.recept.RecipeList;
+import set.products.Product;
+import set.recept.Recipe;
+import set.recept.RecipeList;
 
-import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Random;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
         Product bread = new Product("Хлеб", 30.2f, 2);
         Product milk = new Product("Молоко", 50, 1);
-        ProductList productList = new ProductList();
-        productList.addProducts(bread);
-        productList.addProducts(milk);
-        System.out.println(productList);
-        bread.setBoughtCheck();
-        System.out.println(productList);
+//        ProductList productList = new ProductList();
+//        productList.addProducts(bread);
+//        productList.addProducts(milk);
+//        System.out.println(productList);
+//        bread.setBoughtCheck();
+//        System.out.println(productList);
 
-        Recipe milkBread = new Recipe("Молочный хлеб", Set.of(bread, milk));
-        Recipe milkBread2 = new Recipe("Молочный хлеб", Set.of(bread, milk));
+//        Recipe milkBread2 = new Recipe("молочный хлеб", Set.of(bread, milk));
+
+        Map<Product, Integer> list = new HashMap<>();
+        list.put(bread,2);
+        list.put(milk,1);
         RecipeList recipeList = new RecipeList();
-        recipeList.addRecipe(milkBread);
-        milkBread.getRecipePrice();
-        System.out.println(recipeList);
-        numbersSetRemove();
-        MultiplicationTable multiplicationTable = new MultiplicationTable();
-        System.out.println(multiplicationTable);
+        Recipe milkBread = new Recipe("Молочный хлеб", Set.of(bread, milk));
 
-        Passport den = new Passport(17109,"Petrov", "Den", null, LocalDate.now());
-        Passport den1 = new Passport(53561,"Troynov", "Den", null, LocalDate.now());
-        Passport den2 = new Passport(56434,"Sergeev", "Den", null, LocalDate.now());
-        Passport den3 = new Passport(79543,"Ivanov", "Den", null, LocalDate.now());
-//        Passport noname = new Passport(041234, null, null, null, LocalDate.now() );
-        PassportList passportList = new PassportList();
-        passportList.addOrUpdatePassport(den);
-        passportList.addOrUpdatePassport(den1);
-        passportList.addOrUpdatePassport(den2);
-        passportList.addOrUpdatePassport(den3);
-//        passportList.addOrUpdatePassport(noname);
-        System.out.println(passportList);
-        System.out.println(passportList.findByNumber(17109));
+        recipeList.addRecipe(milkBread);
+        System.out.println(recipeList);
+//        System.out.println(milkBread.getRecipePrice());
+
 //        recipeList.addRecipe(milkBread2);
-//
-//
-//
-//
+
 //        Product milk2 = new Product("Молоко", 100, 2);
 //        productList.addProducts(milk2);
 //        System.out.println(productList);
 
+        HashMap<String, String> phone = new HashMap<>();
+        phone.put("Антон Антонов", "8-921-111-11-11");
+        phone.put("Виталий Витальев", "8-921-222-22-22");
+        phone.put("Борис Борисов", "8-921-333-33-33");
+        phone.put("Герман Германов", "8-921-444-44-44");
+        phone.put("Демьян Демьянов", "8-921-555-55-55");
+        phone.put("Елисей Елисеев", "8-921-666-66-66");
+        phone.put("Жора Жориков", "8-921-777-77-77");
+        phone.put("Зинаида Зинаидова", "8-921-888-88-88");
+        phone.put("Иван Иванов", "8-921-999-99-99");
+        phone.put("Йва Йванова", "8-900-000-00-00");
+        phone.put("Констанитин Констанитнов", "8-900-111-11-11");
+        phone.put("Леонид Леонидов", "8-900-222-22-22");
+        phone.put("Михаил Михаилов", "8-900-333-33-33");
+        phone.put("Николай Николаев", "8-900-444-44-44");
+        phone.put("Ольга Ольгавна", "8-900-555-55-55");
+        phone.put("Петр Петоров", "8-900-666-66-66");
+        phone.put("Роман Романов", "8-900-777-77-77");
+        phone.put("Сегрей Сергеев", "8-900-888-88-88");
+        phone.put("Тимур Тимуров", "8-900-999-99-99");
+        phone.put("Ульяна Ульянова", "8-921-321-48-87");
 
-    }
-    private static void numbersSetRemove() {
-        Set<Integer> numbers = new HashSet<>(   );
-        Random random = new Random();
-        for (int i = 0; i < 20; i++) {
-            numbers.add(random.nextInt(0, 1000));
+        System.out.println(phone.values());
+        System.out.println(phone.get("Ольга Ольгавна"));
+
+        System.out.println();
+
+
+        HashMap<String, List<Integer>> map = new HashMap<>();
+        HashMap<String, Integer> listMap = new HashMap<>();
+        ArrayList<ArrayList<Integer>> list1 = new ArrayList<>(4);
+        for (int i = 0; i < 5; i++) {
+            list1.add(new ArrayList<>());
+            list1.get(i).add((int) (Math.random() * 1000));
+            list1.get(i).add((int) (Math.random() * 1000));
+            list1.get(i).add((int) (Math.random() * 1000));
+            map.put("str" + Integer.toString(i), list1.get(i));
+            System.out.println("str" + Integer.toString(i) + "->" + list1.get(i));
+            listMap.put("str" + Integer.toString(i), list1.get(i).get(0) +
+                    list1.get(i).get(1) + list1.get(i).get(2));
         }
-        System.out.println(numbers);
-        Iterator<Integer> iterator = numbers.iterator();
-        while (iterator.hasNext()) {
-            Integer next = iterator.next();
-            if (next % 2 != 0) {
-                iterator.remove();
-            }
+        for (int i = 0; i < 5; i++) {
+            System.out.println("str" + Integer.toString(i) + "->" + listMap.get("str"
+                    + Integer.toString(i)));
         }
-        System.out.println(numbers);
+
+
+        System.out.println();
+
+        HashMap<Integer, String> integerStringHashMap = new HashMap<>();
+        for (int i = 1; i < 10; i++) {
+            integerStringHashMap.put(i, "str" + Integer.toString(i));
+        }
+        for (int i = 1; i < 10; i++) {
+            System.out.println(" " + integerStringHashMap.get(i));
+        }
+
     }
 
 }
